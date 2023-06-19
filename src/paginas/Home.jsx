c194f8c4db6d3d8ab0053e98b2de9f7e956fe45a
Melina <melina.llacua@gmail.com>
2023-06-19 15:44:47 -0500

    Se realizo los modal con sus respectivas validaciones - Soles y Galones.

--- src/paginas/Home.jsx
+++ src/paginas/Home.jsx
@@ -1,15 +1,28 @@
 import React, { useState } from 'react';
 import LocalGasStationIcon from '@mui/icons-material/LocalGasStation';
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button } from '@mui/material';
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button,TextField } from '@mui/material';
+import InputAdornment from '@material-ui/core/InputAdornment';
 
 const Home = () => {
 
   const [filtroLado, setFiltroLado] = useState('');
-  const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedManguera, setSelectedManguera] = useState(null); 
   const [isLadoMangueraSelected, setIsLadoMangueraSelected] = useState(false);
 
-  const [showAlert, setShowAlert] = useState(false);
+  const [isModalLibreOpen, setIsModalLibreOpen] = useState(false);
+
+  /* Modal de Soles*/
+  const [isModalSolesOpen, setIsModalSolesOpen] = useState(false);
+  const [inputSoles, setSoles] = useState('');
+  const [errorSoles, setErrorSoles] = useState('');
+
+  /* Modal de Galones*/
+  const [isModalGalonesOpen, setIsModalGalonesOpen] = useState(false);
+  const [inputGalones, setGalones] = useState('');
+  const [errorGalones, setErrorGalones] = useState('');
+
+  const [showAlertSuccess, setShowAlertSuccess] = useState(false);
+  const [showAlertError, setShowAlertError] = useState(false);
 
 
   const cards_lados = [
@@ -112,22 +125,208 @@ const Home = () => {
     setIsLadoMangueraSelected(filtroLado !== '' && manguera !== null); 
   };
 
-  const handleModalOpen = () => {
+   /* Modal Libre */
+   const handleModalLibreOpen = () => {
+
     if (isLadoMangueraSelected) {
-      setIsModalOpen(true);
+      setIsModalLibreOpen(true);
     } else {
-      setShowAlert(true);
+      setShowAlertError(true);
+
       setTimeout(() => {
-        setShowAlert(false);
+        setShowAlertError(false);
       }, 1500); 
+
     }
+
   };
   
-  const handleModalClose = () => {
-    setIsModalOpen(false);
+  const handleModalLibreClose = () => {
+    setIsModalLibreOpen(false);
   };
 
+  /* Guardar Modal Libre*/
+
+  const handleSubmitLibre = (e) => {
+
+      setShowAlertSuccess(true);
+
+      setTimeout(() => {
+
+        setShowAlertSuccess(false);
+        handleModalLibreClose();
+        
+      }, 1000);
+
+  };
+
+    
+  /* Abrir o Cerrar Modal de Soles */
+  const handleModalSolesOpen = () => {
+
+    if (isLadoMangueraSelected) {
+
+      resetFormSoles();
+      setIsModalSolesOpen(true);
+
+    } else {
+      setShowAlertError(true);
+
+      setTimeout(() => {
+        setShowAlertError(false);
+      }, 1500); 
+
+    }
+
+  };
+
+  const handleModalSolesClose = () => {
+    setIsModalSolesOpen(false);
+  };
+
+  /* Dar como limites de Chars hasta 10 - Soles*/
+  const handleSolesChange = (e) => {
+
+    const value = e.target.value.replace(/\D/, '').slice(0, 10);
+      setSoles(value);
+      setErrorSoles('');
+
+  };
+
+  /* Validar Modal de soles*/
+  const handleValidationSoles = () => {
+
+    let isValid = true;
+
+    if (inputSoles === '') {
+      setErrorSoles('El campo soles es obligatorio');
+      isValid = false;
+    } else if (inputSoles < 5) {
+      setErrorSoles('El monto debe ser mayor o igual a 5 Soles.');
+      isValid = false;
+    } else if (inputSoles > 9999) {
+      setErrorSoles('El monto debe ser menor o igual a 9999 Soles.');
+      isValid = false;
+    }else {
+      setErrorSoles('');
+    }
+
+    return isValid;
+  };
+
+
+  /* Resetear Modal Soles */
+  const resetFormSoles = () => {
+
+    setSoles('');
+    setErrorSoles('');
+    setShowAlertSuccess(false);
+
+  };
+
+   /* Guardar Modal Soles*/
+
+   const handleSubmit = (e) => {
+
+    e.preventDefault();
   
+    if (handleValidationSoles()) {
+
+      setShowAlertSuccess(true);
+
+      setTimeout(() => {
+
+        setShowAlertSuccess(false);
+        handleModalSolesClose();
+        
+      }, 1000);
+
+    }
+
+  };
+
+  /* Modal Galones */
+  const handleModalGalonesOpen = () => {
+
+    if (isLadoMangueraSelected) {
+
+      resetFormGalones();
+      setIsModalGalonesOpen(true);
+
+    } else {
+      setShowAlertError(true);
+
+      setTimeout(() => {
+        setShowAlertError(false);
+      }, 1500); 
+
+    }
+
+  };
+  
+  const handleModalGalonesClose = () => {
+    setIsModalGalonesOpen(false);
+  };
+  
+  /* Dar como limites de Chars hasta 10 - Galones*/
+  const handleGalonesChange = (e) => {
+
+    const value = e.target.value.replace(/\D/, '').slice(0, 10);
+      setGalones(value);
+      setErrorGalones('');
+
+  };
+
+  /* Validar Modal de Galones*/
+  const handleValidationGalones = () => {
+
+    let isValid = true;
+
+    if (inputGalones === '') {
+      setErrorGalones('El campo galones es obligatorio');
+      isValid = false;
+    } else if (inputGalones < 5) {
+      setErrorGalones('El monto debe ser mayor o igual a 5 Galones.');
+      isValid = false;
+    } else if (inputGalones > 9999) {
+      setErrorGalones('El monto debe ser menor o igual a 9999 Galones.');
+      isValid = false;
+    }else {
+      setErrorGalones('');
+    }
+
+    return isValid;
+  };
+
+   /* Resetear Modal Galones */
+   const resetFormGalones = () => {
+
+    setGalones('');
+    setErrorGalones('');
+    setShowAlertSuccess(false);
+
+  };
+
+   /* Guardar Modal Galones*/
+
+  const handleSubmitGalones = (e) => {
+
+    e.preventDefault();
+  
+    if (handleValidationGalones()) {
+
+      setShowAlertSuccess(true);
+
+      setTimeout(() => {
+
+        setShowAlertSuccess(false);
+       handleModalGalonesClose();
+        
+      }, 1000);
+
+    }
+
+  };
 
   return (
     <div>
@@ -179,19 +378,19 @@ const Home = () => {
           
         </div>
 
-        {showAlert && (
-          <div className={`floating-alert ${showAlert ? 'show' : ''}`}>
-            <p className='alert-text'>Seleccionar lado y manguera.</p>
+        {showAlertError && (
+          <div className={`floating-alertError ${showAlertError ? 'show' : ''}`}>
+            <p className='alert-error'>Seleccionar lado y manguera.</p>
           </div>
         )}
-          
+             
         {/* Campo de Modalidad*/}
         <div className="card">
             <h3 className="inner-title">Modalidad</h3>
             <div className="inner-cards">
-                <button className='btn_cards' onClick={handleModalOpen}>LIBRE</button>
-                <button className='btn_cards'>SOLES</button>
-                <button className='btn_cards'>GALONES</button>
+                <button className='btn_cards' onClick={handleModalLibreOpen}>LIBRE</button>
+                <button className='btn_cards' onClick={handleModalSolesOpen}>SOLES</button>
+                <button className='btn_cards' onClick={handleModalGalonesOpen}>GALONES</button>
                 </div>
         </div>
 
@@ -214,16 +413,97 @@ const Home = () => {
           <h3 className="inner-title">Transacciones</h3>
         </div>
 
-        <Dialog open={isModalOpen} onClose={handleModalClose}>
+        <Dialog open={isModalLibreOpen} onClose={handleModalLibreClose}>
           <DialogTitle>Libre</DialogTitle>
           <DialogContent>
             <p>¿Desea activar el modo LIBRE?</p>
           </DialogContent>
+
+          {showAlertSuccess && (
+              <div className={`floating-alertSuccess ${showAlertSuccess ? 'show' : ''}`}>
+                <p className="alert-success">Se guardó correctamente</p>
+              </div>
+            )}
           <DialogActions>
-            <Button onClick={handleModalClose}>Cancelar</Button>
-            <Button >Aceptar</Button>
+            <Button onClick={handleModalLibreClose}>Cancelar</Button>
+            <Button onClick={handleSubmitLibre}>Aceptar</Button>
           </DialogActions>
         </Dialog>
+
+        <Dialog open={isModalSolesOpen} onClose={handleModalSolesClose}>
+
+          <DialogTitle>Soles</DialogTitle>
+
+            <DialogContent>
+
+            {(errorSoles ) && (
+             <div className={`floating-alertErrorInput ${errorSoles ? 'show' : ''}`}>
+                {errorSoles && <p className="alert-errorInput">{errorSoles}</p>}
+              </div>
+            )}
+
+              <TextField type="text" className='campo_input' value={inputSoles} onChange={handleSolesChange} margin="normal" label="Soles" fullWidth
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start" style={{ userSelect: 'none' }}>
+                      S/
+                    </InputAdornment>
+                  ),
+                }}
+              />  
+
+            {showAlertSuccess && (
+              <div className={`floating-alertSuccess ${showAlertSuccess ? 'show' : ''}`}>
+                <p className="alert-success">Se guardó correctamente</p>
+              </div>
+            )}
+
+            </DialogContent>
+
+          <DialogActions>
+            <Button onClick={handleModalSolesClose}>Cancelar</Button>
+            <Button  onClick={handleSubmit} >Aceptar</Button>
+          </DialogActions>
+
+        </Dialog>
+
+
+        <Dialog open={isModalGalonesOpen} onClose={handleModalGalonesClose}>
+
+          <DialogTitle>Galones</DialogTitle>
+
+            <DialogContent>
+
+            {(errorGalones ) && (
+            <div className={`floating-alertErrorInput ${errorGalones ? 'show' : ''}`}>
+                {errorGalones && <p className="alert-errorInput">{errorGalones}</p>}
+              </div>
+            )}
+
+              <TextField type="text" className='campo_input' value={inputGalones} onChange={handleGalonesChange} margin="normal" label="Galones" fullWidth
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start" style={{ userSelect: 'none' }}>
+                      S/
+                    </InputAdornment>
+                  ),
+                }}
+              />  
+
+            {showAlertSuccess && (
+              <div className={`floating-alertSuccess ${showAlertSuccess ? 'show' : ''}`}>
+                <p className="alert-success">Se guardó correctamente</p>
+              </div>
+            )}
+
+            </DialogContent>
+
+          <DialogActions>
+            <Button onClick={handleModalGalonesClose}>Cancelar</Button>
+            <Button  onClick={handleSubmitGalones} >Aceptar</Button>
+          </DialogActions>
+
+          </Dialog>
     </div>
   );
 };
