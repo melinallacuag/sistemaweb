127e70989c2be2d9dc7bbc5c5fc1c26fe610c5f7
Melina <melina.llacua@gmail.com>
2023-06-27 16:11:52 -0500

    Se realizo el modal de inicio de dia y cambio de turno.

--- src/App.js
+++ src/App.js
@@ -17,9 +17,6 @@ function App() {
         <Routes>
             <Route path="/" element={ <Login /> } />
             <Route path="/home" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            
         </Routes>
 
     </Router>
--- src/componentes/Sidebar.jsx
+++ src/componentes/Sidebar.jsx
@@ -2,14 +2,92 @@ import React, { useState } from 'react';
 import logo from '../imagen/logo_appsven.png';
 import mobileLogo  from '../imagen/icon_app.png'
 import LocalGasStationIcon from '@mui/icons-material/LocalGasStation';
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button,TextField } from '@mui/material';
 
 const Sidebar = ({ isOpen }) => {
 
   const [isMobileSidebarOpen, setIsMobileSidebarOpen] = useState(false);
+  const [isModalCTurnoOpen, setIsModalCTurnoOpen] = useState(false);
+  const [isModalIDiaOpen, setIsModalIDiaOpen] = useState(false);
+  const [authenticated, setauthenticated] = useState(null);
+
+   /* Alerta de Correcto o Error*/
+   const [showAlertSuccess, setShowAlertSuccess] = useState(false);
+   const [showAlertError, setShowAlertError] = useState(false);
 
   const toggleMobileSidebar = () => {
     setIsMobileSidebarOpen(!isMobileSidebarOpen);
   };
+
+    /* Modal Cambio de Turno */
+    const handleModalCTurnoOpen = () => {
+
+        setIsModalCTurnoOpen(true);
+      
+        setShowAlertError(true);
+  
+        setTimeout(() => {
+          setShowAlertError(false);
+        }, 1500); 
+  
+    };
+    
+    const handleModalCTurnoClose = () => {
+      setIsModalCTurnoOpen(false);
+    };
+  
+    /* Guardar Modal Cambio de Turno*/
+    const handleSubmitCTurno = (e) => {
+
+        setShowAlertSuccess(true);
+  
+        setTimeout(() => {
+
+          localStorage.removeItem("authenticated");
+          setauthenticated(null);
+          window.location.href = '/'; 
+
+          setShowAlertSuccess(false);
+          handleModalCTurnoClose();
+          
+        }, 1000);
+  
+    };
+    
+      /* Modal Inicio de Día */
+      const handleModalIDiaOpen = () => {
+
+        setIsModalIDiaOpen(true);
+      
+        setShowAlertError(true);
+  
+        setTimeout(() => {
+          setShowAlertError(false);
+        }, 1500); 
+  
+    };
+    
+    const handleModalIDiaClose = () => {
+      setIsModalIDiaOpen(false);
+    };
+  
+    /* Guardar Modal Inicio de Día */
+    const handleSubmitIDia = (e) => {
+
+        setShowAlertSuccess(true);
+  
+        setTimeout(() => {
+
+          localStorage.removeItem("authenticated");
+          setauthenticated(null);
+          window.location.href = '/'; 
+
+          setShowAlertSuccess(false);
+          handleModalIDiaClose();
+          
+        }, 1000);
+  
+    };
 
   return (
     <div>
@@ -25,7 +103,7 @@ const Sidebar = ({ isOpen }) => {
             <li className='sidebar_li activo'>
               <a href="/" className='sidebar_menu'>
               <LocalGasStationIcon className='sidebar_icon'/>
-                  <span>Home</span>
+                  <span>Venta</span>
                   
               </a>
             </li>
@@ -37,15 +115,58 @@ const Sidebar = ({ isOpen }) => {
             <li className='sidebar_li'>
               <a href="/about" className='sidebar_menu'>
               <LocalGasStationIcon className='sidebar_icon'/>
-              <span>About</span>
+              <span>Cierre X</span>
               </a>
             </li>
 
             <li className='sidebar_li'>
-              <a href="/contact" className='sidebar_menu'>
+              <a href="#" className='sidebar_menu' onClick={handleModalCTurnoOpen}>
               <LocalGasStationIcon className='sidebar_icon'/>
-              <span>Contact</span></a>
+              <span>Cambio de Turno</span>
+              </a>
             </li>
+
+            <li className='sidebar_li'>
+              <a href="#" className='sidebar_menu' onClick={handleModalIDiaOpen}>
+              <LocalGasStationIcon className='sidebar_icon'/>
+              <span>Inicio de Día</span></a>
+            </li>
+
+                {/* Modal Cambio de Turno*/}
+                <Dialog open={isModalCTurnoOpen} onClose={handleModalCTurnoClose}>
+                  <DialogTitle>Cambio de Turno</DialogTitle>
+                  <DialogContent>
+                    <p>¿Desea generar el cambio de turno?</p>
+                  </DialogContent>
+
+                  {showAlertSuccess && (
+                      <div className={`floating-alertSuccess ${showAlertSuccess ? 'show' : ''}`}>
+                        <p className="alert-success">Se genero correctamente</p>
+                      </div>
+                    )}
+                  <DialogActions>
+                    <Button variant="contained" color="secondary" style={{ backgroundColor: '#ff2b2b' }} onClick={handleModalCTurnoClose}>Cancelar</Button>
+                    <Button variant="contained" color="primary" onClick={handleSubmitCTurno}>Aceptar</Button>
+                  </DialogActions>
+                </Dialog>
+
+                {/* Modal Inicio de Día*/}
+                <Dialog open={isModalIDiaOpen} onClose={handleModalIDiaClose}>
+                  <DialogTitle>Inicio de Dia</DialogTitle>
+                  <DialogContent>
+                    <p>¿Desea generar inicio de día?</p>
+                  </DialogContent>
+
+                  {showAlertSuccess && (
+                      <div className={`floating-alertSuccess ${showAlertSuccess ? 'show' : ''}`}>
+                        <p className="alert-success">Se genero correctamente</p>
+                      </div>
+                    )}
+                  <DialogActions>
+                    <Button variant="contained" color="secondary" style={{ backgroundColor: '#ff2b2b' }} onClick={handleModalIDiaClose}>Cancelar</Button>
+                    <Button variant="contained" color="primary" onClick={handleSubmitIDia}>Aceptar</Button>
+                  </DialogActions>
+                </Dialog>
 
           <hr class="sidebar-divider my-0"></hr>
 
